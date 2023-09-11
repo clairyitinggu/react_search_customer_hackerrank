f5903104cd6028f0c7248c1e4c7fa16614f6b51d
Clair Gu <54907294+clairyitinggu@users.noreply.github.com>
2023-09-11 05:00:08 +0000

    Update 3 files

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,39 +1,38 @@
-import React from "react";
+import React from 'react'
 
-function CustomerList({ customers }) {
+function CustomerList({customers}) {
   return (
-    <div className="layout-column align-items-center justify-content-start">
-      {!customers.length && <p data-testid="no-results">No Results Found!</p>}
-      <div className="card w-40 pt-30 pb-8 mt-20">
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Age</th>
-              <th>Location</th>
-              <th>Gender</th>
-              <th>Income</th>
-            </tr>
-          </thead>
-          {customers.length && (
-            <tbody data-testid="searched-customers">
-              {customers?.map(
-                ({ name, age, location, gender, income }, index) => (
-                  <tr key={index}>
-                    <td>{name}</td>
-                    <td>{age}</td>
-                    <td>{location}</td>
-                    <td>{gender}</td>
-                    <td>{income}</td>
-                  </tr>
-                )
-              )}
-            </tbody>
-          )}
-        </table>
-      </div>
+    <div className='layout-column align-items-center justify-content-start'>
+       {customers.length === 0 && <p data-testid='no-results'>No Results Found!</p>}
+        <div className='card w-40 pt-30 pb-8 mt-20'>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Location</th>
+                        <th>Gender</th>
+                        <th>Income</th>
+                    </tr>
+                    </thead>
+                    <tbody data-testid='searched-customers'>
+                      {customers.length && customers?.map(({name,age,location, gender, income},index) =>
+                        <tr key={index}>
+                        <td>{name}</td>
+                        <td>{age}</td>
+                        <td>{location}</td>
+                        <td>{gender}</td>
+                        <td>{income}</td>
+                    </tr>
+                        
+                        )}
+                            
+                    </tbody>
+                </table>
+            </div>
+
     </div>
-  );
+  )
 }
 
 export default CustomerList;
--- src/components/SearchCustomer.js
+++ src/components/SearchCustomer.js
@@ -1,26 +1,37 @@
-import React from "react";
-import CustomerList from "./CustomerList";
-import { useSearch } from "../hooks/useSearch";
-import customers from "../List";
+import React, { useState } from 'react'
+import CustomerList from './CustomerList'
+import List from '../List';
+import customers from '../List';
 
 function SearchCustomer() {
-  const { searchedCustomers, handleSearch, search } = useSearch({ customers });
+  const [search, setSearch] = useState("");
+  const [suggestions, setSuggestions] = useState(customers)
+
+  const handleInputChange = (e)=>{
+    const value = e.target.value;
+    setSearch(value);
+
+    if(value){
+      const filteredCustomers = customers.filter((item) =>
+      Object.values(item).some((value) =>
+        value.toString().split(" ")[0].startsWith(search)
+      )
+    );
+    setSuggestions(filteredCustomers)
+    }else{
+      setSuggestions([]);
+    }
+  
+  }
 
   return (
     <>
-      <div className="layout-row align-items-center justify-content-center mt-30">
-        <input
-          className="large mx-20 w-20"
-          data-testid="search-input"
-          type="text"
-          value={search}
-          onChange={handleSearch}
-          placeholder="Enter search term (Eg: Phil)"
-        />
-      </div>
-      <CustomerList customers={searchedCustomers} />
+    <div className='layout-row align-items-center justify-content-center mt-30'>
+        <input className='large mx-20 w-20' data-testid='search-input' value={search} onChange={handleInputChange} placeholder='Enter search term (Eg: Phil)' />
+    </div>
+    <CustomerList customers={suggestions} />
     </>
-  );
+  )
 }
 
-export default SearchCustomer;
+export default SearchCustomer
